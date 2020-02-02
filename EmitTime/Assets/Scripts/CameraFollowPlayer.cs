f1e70bf7d207787e72d911401e7c488cbf4db11d
Aleix <aleix.barreto01@estudiant.upf.edu>
2020-02-02 21:16:05 +0100

    import new project

---
+++ EmitTime/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Text time_text;    
+
+    private Vector3 prev_pos;
+
+    void Start()
+    {
+        prev_pos = new Vector3(0,0,0);
+    }
+
+    void Update()
+    {
+        //Don't have prev pos
+        bool has_moved;
+
+        if(prev_pos == new Vector3(0,0,0))
+            has_moved = false;
+        else if(prev_pos.x == Manager.Instance.player.transform.position.x){
+            has_moved = false;
+        }else
+            has_moved = true;
+
+        float x_diff = Manager.Instance.player.transform.position.x - prev_pos.x;
+
+        if(has_moved && !Manager.Instance.is_inverted)
+            Manager.Instance.time += x_diff; 
+        else if(has_moved && Manager.Instance.is_inverted)
+            Manager.Instance.time -= x_diff;
+        
+        time_text.text = "Time: " + Math.Floor(Manager.Instance.time).ToString();
+
+        prev_pos = Manager.Instance.player.transform.position;
+
+        if(Camera.main != null)
+        {
+            Vector3 camera_pos = Camera.main.transform.position;
+            camera_pos.x = Manager.Instance.player.transform.position.x;
+            camera_pos.y = Manager.Instance.player.transform.position.y;
+            Camera.main.transform.position = camera_pos;
+        }
+
+        if(Input.GetButtonDown("Invert") && Camera.main != null){
+
+            Manager.Instance.is_inverted = !Manager.Instance.is_inverted;
+
+            Camera.main.transform.Rotate(0.0f, 180.0f, 0.0f, Space.Self);
+            Vector3 camera_pos = Camera.main.transform.position;
+            camera_pos.z *= -1;
+            Camera.main.transform.position = camera_pos;
+        }
+
+
+    }
+}
---
+++ EmitTime/Assets/Scripts/Manager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Manager : MonoBehaviour
+{
+	public static Manager Instance {get; private set;}
+    public float time;
+    public GameObject player;
+    public bool is_inverted;
+
+    private void Awake()
+    {
+    	if(Instance == null)
+    	{
+    		Instance = this;
+    		time = 0;
+    		player = GameObject.Find("Character");
+    		is_inverted = false;
+    	}
+    }
+}
