9e02e043c986c8747fd648806a073c567aa644af
Juan Marquerie <jsmarquerie@gmail.com>
2020-02-11 17:33:56 +0100

    Finsihed Time deterioration

--- EmitTime/Assets/Scripts/TimeDeterioration.cs
+++ EmitTime/Assets/Scripts/TimeDeterioration.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class TimeDeterioration : MonoBehaviour
 {
-    private Transform current_pos;
+    private Transform current_pos;    
     private Vector3 start_position;
     private Vector3 end_position;
+
+    private Vector3 start_scale;
+    private Vector3 end_scale;
+
+    private float start_rotation;
+    private float end_rotation;
     
-    public Transform end_state;
+    public Transform end_transf;
     public int start_time;
     public int end_time;
     // Start is called before the first frame update
@@ -16,27 +22,39 @@ public class TimeDeterioration : MonoBehaviour
     {
         current_pos = this.GetComponent<Transform>();
         start_position = current_pos.position;
-        end_position = end_state.position;
+        start_scale = current_pos.localScale;
+        start_rotation = current_pos.eulerAngles.z;
+
+        end_position = end_transf.position;
+        end_scale = end_transf.localScale;
+        end_rotation = end_transf.eulerAngles.z;
     }
     
     float LERP(float x, float x1, float x2, float f1, float f2) {
-        return f1 + ((f2 / f1) / (x2 - x1)) * (x - x1);
+        return f1 + ((f2 - f1) / (x2 - x1 + 0.0001f)) * (x - x1);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float curr_time = Manager.Instance.time;
 
         if (curr_time > start_time && curr_time < end_time) {
             Vector3 new_position = new Vector3(LERP(curr_time, start_time, end_time, start_position.x, end_position.x),
                                             LERP(curr_time, start_time, end_time, start_position.y, end_position.y),
-                                           start_position.z);
+                                            start_position.z);
+
+            Vector3 new_scale =  new Vector3(LERP(curr_time, start_time, end_time, start_scale.x, end_scale.x),
+                                            LERP(curr_time, start_time, end_time, start_scale.y, end_scale.y),
+                                            LERP(curr_time, start_time, end_time, start_scale.z, end_scale.z));
+
+            Vector3 new_rotation = new Vector3(0, 0,
+                                            LERP(curr_time, start_time, end_time, start_rotation, end_rotation));
             
-            Debug.Log(curr_time +"="+start_time+"="+ end_time+"="+new_position);
+            Debug.Log(curr_time +"="+start_position+"="+ end_position+"="+new_position);
             current_pos.position = new_position;
-            //current_pos.eulerAngles = new_rotation;
-            //current_pos.localScale = new_scale;
+            current_pos.eulerAngles = new_rotation;
+            current_pos.localScale = new_scale;
         }
     }
 }
