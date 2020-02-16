6eb2570ffb4fe49a41b086c80b752731a9b22e7d
Rob <robert.garcia03@estudiant.upf.edu>
2020-02-16 21:11:10 +0100

    button and door v.1

---
+++ EmitTime/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+
+	public GameObject door;
+    private void OnTriggerEnter2D(Collider2D collider)
+	{
+		door.SetActive(false);
+	}
+
+	private void OnTriggerStay2D(Collider2D collider)
+	{
+		Debug.Log("Trigger!");
+		door.SetActive(false);
+	}
+
+	private void OnTriggerExit2D(Collider2D collider)
+	{
+		Debug.Log("Trigger!");
+		door.SetActive(true);
+	}
+
+}
