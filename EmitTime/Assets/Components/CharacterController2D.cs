929a13c2e7f8287787798d8aee44991d3b7bcf13
Juan Marquerie <jsmarquerie@gmail.com>
2020-03-25 16:36:09 +0100

    Fixed movement

--- EmitTime/Assets/Components/CharacterController2D.cs
+++ EmitTime/Assets/Components/CharacterController2D.cs
@@ -54,24 +54,11 @@ public class CharacterController2D : MonoBehaviour
 	public void Move(float move, bool jump)
 	{
 		//only control the player if grounded or airControl is turned on
-		if (m_Grounded)
-		{
-			// Move the character by finding the target velocity
-			Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
-			// And then smoothing it out and applying it to the character
-			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
-
-			// If the input is moving the player right and the player is facing left...
-			if (move > 0 && !m_FacingRight)
-			{
-				//Flip();
-			}
-			// Otherwise if the input is moving the player left and the player is facing right...
-			else if (move < 0 && m_FacingRight)
-			{
-				//Flip();
-			}
-		}
+		// Move the character by finding the target velocity
+		Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+		// And then smoothing it out and applying it to the character
+		m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+		
 		// If the player should jump...
 		if (m_Grounded && jump)
 		{
