bd54aac87f27b3362af645f11a5882bab7896669
Juan Marquerie <jsmarquerie@gmail.com>
2020-03-28 13:27:10 +0100

    Fixed foor

--- EmitTime/Assets/Scripts/DeteriorateScripts/DeteriorateDoor.cs
+++ EmitTime/Assets/Scripts/DeteriorateScripts/DeteriorateDoor.cs
@@ -9,6 +9,8 @@ public class DeteriorateDoor : MonoBehaviour
     public int start_time;
     public int end_time; 
     public int deactivate_frame;
+
+    public bool needs_button = true;
     
     private Animator anim;
     private Rigidbody2D rb;
@@ -26,9 +28,19 @@ public class DeteriorateDoor : MonoBehaviour
         return f1 + ((f2 - f1) / (x2 - x1 + 0.0001f)) * (x - x1);
     }
 
+    bool checkPlayerPosition(){
+        return Mathf.Floor(Manager.Instance.end_door.transform.position.x) == Mathf.Floor(Manager.Instance.player.transform.position.x) && 
+            (Mathf.Floor(Manager.Instance.end_door.transform.position.y) + 2.5 >  Mathf.Floor(Manager.Instance.player.transform.position.y) &&
+                Mathf.Floor(Manager.Instance.end_door.transform.position.y) - 2.5 <  Mathf.Floor(Manager.Instance.player.transform.position.y));
+            
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if (needs_button) {
+            Manager.Instance.end_door_active = button.isPressed;
+        }
         if (Manager.Instance.time >= start_time) {
             int index = (int) LERP(Manager.Instance.time, start_time, end_time, start_index, end_index);
             
@@ -36,21 +48,16 @@ public class DeteriorateDoor : MonoBehaviour
                 index = end_index;
             }
             
-            /*if (index >= deactivate_frame) {
-                //rb.enabled = false;
-                //rb.isKinematic = false;
-                rb.constraints = RigidbodyConstraints2D.FreezeAll;
-                rb.gravityScale = 0f;
-                box_collider.enabled = false;
+            anim.SetInteger("DoorTime", index);
+            if (needs_button) {
+                anim.SetBool("ButtonPressed", button.isPressed);
+            } else if (checkPlayerPosition()) {
+                // If it does not need the button, open the door when the player is near
+                anim.SetBool("ButtonPressed", true);
+                Manager.Instance.end_door_active = true;
             } else {
-                //rb.enabled = true;
-                //rb.isKinematic = true;
-                rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-                rb.gravityScale = 1f;
-                box_collider.enabled = true;
-            }*/
-            anim.SetInteger("DoorTime",index);
-            anim.SetBool("ButtonPressed", button.isPressed);
+                Manager.Instance.end_door_active = false;
+            }
         }
     }
 }
